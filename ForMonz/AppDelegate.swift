8b1d96335b1428334b757988b9b953cc0483eca1
Joan Cardona <joancardona.17@gmail.com>
2017-01-21 19:05:06 +0000

    login with monzo

--- ForMonz/AppDelegate.swift
+++ ForMonz/AppDelegate.swift
@@ -40,6 +40,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        print("I am opening from an url! \(url)")
+        return true
+    }
 
 
 }
---
+++ ForMonz/MonzoLoginViewController.swift
@@ -0,0 +1,64 @@
+//
+//  MonzoLoginViewController.swift
+//  ForMonz
+//
+//  Created by Joan Cardona on 11/01/17.
+//  Copyright © 2017 Joan. All rights reserved.
+//
+
+import UIKit
+
+class MonzoLoginViewController: UIViewController {
+
+    var state: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        state = randomString(length: 8)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    /*
+ 
+     "https://auth.getmondo.co.uk/?
+     client_id=$client_id&
+     redirect_uri=$redirect_uri&
+     response_type=code&
+     state=$state_token"
+     
+     */
+    
+
+    func randomString(length: Int) -> String {
+        
+        let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let len = UInt32(letters.length)
+        
+        var randomString = ""
+        
+        for _ in 0 ..< length {
+            let rand = arc4random_uniform(len)
+            var nextChar = letters.character(at: Int(rand))
+            randomString += NSString(characters: &nextChar, length: 1) as String
+        }
+        
+        return randomString6
+    }
+    
+    @IBAction func monzoLoginTapped(_ sender: AnyObject) {
+        
+        let stringURL = "https://auth.getmondo.co.uk/?client_id=oauthclient_00009GI8qie279LfSMFBuT&redirect_uri=flacez://authorized&response_type=code&state=\(state)"
+        
+        let url = URL(string: stringURL)!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    
+    }
+
+}
